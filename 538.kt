0baca351e9310d4866e35387e35299fef67d6c08
Your Name <gim.kobayashi@gmail.com>
2017-07-08 14:06:13 +0900

    a

--- 538.kt
+++ 538.kt
@@ -1,9 +1,9 @@
-
+import java.lang.Math
 fun main( args : Array<String> ) {
   val (a, b, c) = readLine()!!.split(" ").map { it.toLong() }
   // c = n * b + d
   // b = n * a + d
   val n = (c - b) / (b - a).toDouble() 
   val d = c - n * b
-  println("${(n*c + d).toLong()}")
+  println("${Math.round(n*c + d)}")
 }
