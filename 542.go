81dbd3e7ab79f38e68da83d9d73bbd2957bfedf1
gimpei <gim.kobayashi@gmail.com>
2017-07-20 02:14:32 +0900

    nnn

--- 542.go
+++ 542.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -17,5 +19,22 @@ func read() string {
 
 func main() {
 	ii := strings.Split(read(), " ")
-	fmt.Printf("%s %s\n", ii[1], ii[0])
+	i1, _ := strconv.Atoi(ii[0])
+	i2, _ := strconv.Atoi(ii[1])
+	m := make(map[int]int)
+	for e1 := 0; e1 <= i1; e1++ {
+		for e2 := 0; e2 <= i2; e2++ {
+			m[e1+e2*5] = 0
+		}
+	}
+	ks := make([]int, 0)
+	for k, _ := range m {
+		ks = append(ks, k)
+	}
+	sort.Ints(ks)
+	for _, k := range ks {
+		if k != 0 {
+			fmt.Println(k)
+		}
+	}
 }
