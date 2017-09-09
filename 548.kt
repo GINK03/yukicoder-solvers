c882aa602a6f6b3c022cadf9be4898dc4805b78f
gink03 <gim.kobayashi@gmail.com>
2017-09-09 17:18:47 +0000

    cross

--- 548.kt
+++ 548.kt
@@ -1,21 +1,25 @@
 
 fun main( args : Array<String> ) {
-  val a = readLine()!!
+  val bs = readLine()!!
   "abcdefghijklm".map { x ->
-    val x2 = (a + x.toString()).groupBy {
-      it
-    }.toList()
-    val a1 = x2.map { it.first }.sortedBy{ it }.joinToString("")
-    val a2 = x2.map { it.second.size }.sortedBy { it }.joinToString("")
-    val c1 = a1.length == 13
-    val c2 = when { c1 -> a1.slice(0..11) == "abcdefghijklm"; else -> false }
-    val c3 = a2 == "1111111111112"
-    println("a")
-    println(a1)
-    println(a1.slice(0..11) )
-    println(c1)
-    println(c2)
-    println(c3)
-    println(x2)
+    val mm = (bs + x.toString())
+      .toList()
+      .groupBy { 
+        it
+      }.map { 
+        val (k, arr) = it
+        Pair(k, arr.size)
+      }.toMap()
+    val res = "abcdefghijklm".map {
+      mm[it]
+    }.sortedBy { 
+      it 
+    }.let { 
+      when { 
+        it.joinToString("") == "1111111111112" -> x
+        else -> null
+      }
+    }
+    res
   }
 }
