903138e47f877e5d0fc36c23907bf25a8af71aa1
gimpei <gim.kobayashi@gmail.com>
2020-01-01 12:41:56 +0900

    update

---
+++ 944.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int N; cin >> N;
+  int a[N];
+  int s=0;
+  for(int i=0;i<N;i++) { cin>>a[i]; }
+  for(int i=0, j=0; j<N; i = j) {
+  }
+}
