b6a12b55991d8ace76aedb5349e3bac4c5058845
gimpei <gim.kobayashi@gmail.com>
2020-03-29 13:49:09 +0900

    solve 1005

---
+++ 1005.py
@@ -0,0 +1,15 @@
+
+s = list(input())
+t = list(input())
+
+if len(t) == 1 and ''.join(t) in ''.join(s):
+    print(-1)
+    exit()
+
+c = 0 
+while c < len(s):
+    if s[c:c+len(t)] == t:
+        s.insert(c+len(t)-1, '.')
+    c += 1
+
+print(''.join(s).count('.'))
