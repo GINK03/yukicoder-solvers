412a6cec8b6b61d5e7d4e245d35d76b7d574f887
gimpei <gim.kobayashi@gmail.com>
2020-03-30 12:02:12 +0900

    solve 0999

--- 0999.py
+++ 0999.py
@@ -1,11 +1,28 @@
+N = int(input())
 
+ax = list(map(int, input().split()))
 
-xs = list(map(int, input().split()))
-xs = sorted(xs)
-m = xs[0]
-xs = list(map(lambda x:x - m, xs))
+rights = []
+for i in range(0, N*2, 2):
+    right = ax[i] - ax[i+1]
+    rights.append(right)
 
-if xs == [0, 1, 2, 3]:
-    print("Yes")
-else:
-    print("No")
+lefts = []
+for i in range(0, N*2, 2):
+    left = ax[i+1] - ax[i]
+    lefts.append(left)
+
+a = 0
+for i, r in enumerate(list(rights)):
+    a += r
+    rights[i] = a
+
+a = 0
+for i, l in enumerate(list(reversed(lefts))):
+    a += l
+    lefts[i] = a
+lefts = list(reversed(lefts))
+rights.insert(0, 0)
+lefts.append(0)
+
+print(max([r+l for r,l in zip(rights, lefts)]))
